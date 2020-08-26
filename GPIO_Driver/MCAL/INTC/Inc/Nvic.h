ba67950cffc30f9e75210fdc15744a7a50c19971
Mjihan05 <michaeljihan05@gmail.com>
2020-08-27 00:46:08 +0530

    INTC - Created a few functions for NVIC

--- GPIO_Driver/MCAL/INTC/Inc/Nvic.h
+++ GPIO_Driver/MCAL/INTC/Inc/Nvic.h
@@ -12,6 +12,15 @@
 #ifndef INTC_INC_NVIC_H_
 #define INTC_INC_NVIC_H_
 
+#define TOTAL_NO_OF_INTERRUPTS		(82U)
 
+typedef uint8_t IRQn_t;
+
+void NVIC_EnableIRQ(IRQn_t IRQn);
+void NVIC_DisableIRQ(IRQn_t IRQn);
+uint32_t NVIC_GetPendingIRQ (IRQn_t IRQn);
+void NVIC_SetPendingIRQ (IRQn_t IRQn);
+void NVIC_ClearPendingIRQ (IRQn_t IRQn);
+uint32_t NVIC_GetActive (IRQn_t IRQn);
 
 #endif /* INTC_INC_NVIC_H_ */
--- GPIO_Driver/MCAL/INTC/Src/Nvic.c
+++ GPIO_Driver/MCAL/INTC/Src/Nvic.c
@@ -9,18 +9,100 @@
  *
  * ********************************************/
 
+#include <stdint.h>
+#include <Reg_Macros.h>
+
+#include "NVIC_regTypes.h"
+#include "Nvic.h"
 
 
+void NVIC_EnableIRQ(IRQn_t IRQn)
+{
+	if(IRQn >= TOTAL_NO_OF_INTERRUPTS)
+	{
+		return;
+	}
 
+	uint8_t regNo = IRQn/32;
+	uint8_t regPos = IRQn%32;
 
+	NVIC.ISER[regNo] |= SET_BIT(regPos);
 
+}
 
+void NVIC_DisableIRQ(IRQn_t IRQn)
+{
+	if(IRQn >= TOTAL_NO_OF_INTERRUPTS)
+	{
+		return;
+	}
 
+	uint8_t regNo = IRQn/32;
+	uint8_t regPos = IRQn%32;
 
+	NVIC.ICER[regNo] |= SET_BIT(regPos);
 
+}
 
+uint32_t NVIC_GetPendingIRQ (IRQn_t IRQn)
+{
+	if(IRQn >= TOTAL_NO_OF_INTERRUPTS)
+	{
+		return 0xFFFFFFFF;
+	}
 
+	uint8_t regNo = IRQn/32;
+	uint8_t regPos = IRQn%32;
 
+	if(NVIC.ISPR[regNo] & MASK_BIT(regPos))
+	{
+		return TRUE;
+	}
+	return FALSE;
+}
+
+void NVIC_SetPendingIRQ (IRQn_t IRQn)
+{
+	if(IRQn >= TOTAL_NO_OF_INTERRUPTS)
+	{
+		return;
+	}
+
+	uint8_t regNo = IRQn/32;
+	uint8_t regPos = IRQn%32;
+
+	NVIC.ISPR[regNo] |= SET_BIT(regPos);
+}
+
+void NVIC_ClearPendingIRQ (IRQn_t IRQn)
+{
+	if(IRQn >= TOTAL_NO_OF_INTERRUPTS)
+	{
+		return;
+	}
+
+	uint8_t regNo = IRQn/32;
+	uint8_t regPos = IRQn%32;
+
+	NVIC.ICPR[regNo] |= SET_BIT(regPos);
+}
+
+uint32_t NVIC_GetActive (IRQn_t IRQn)
+{
+	if(IRQn >= TOTAL_NO_OF_INTERRUPTS)
+	{
+		return FALSE;
+	}
+
+	uint8_t regNo = IRQn/32;
+	uint8_t regPos = IRQn%32;
+
+	if(NVIC.IABR[regNo] & MASK_BIT(regPos))
+	{
+		return TRUE;
+	}
+	return FALSE;
+}
 
 
 
